aea607898dc32d776e0804ce227e767121719a36
Daria Sukhonina <dariasukhonina@gmail.com>
2025-05-20 15:52:13 +0300

    chore: add broadcast test

--- executor/tests/all.rs
+++ executor/tests/all.rs
@@ -1,6 +1,6 @@
 use colorless::Stackify;
 use colorless_executor::{Executor, ExecutorConfig, block_on};
-use futures_lite::future::yield_now;
+use futures_lite::{StreamExt, future::yield_now};
 
 #[test]
 fn simple() {
@@ -12,6 +12,19 @@ fn simple() {
             let t2 = exec.spawn(|| yield_now().await_().unwrap());
             block_on(t1);
             block_on(t2);
+        },
+    )
+    .unwrap();
+}
+
+#[test]
+fn broadcast() {
+    Executor::build_scoped(
+        ExecutorConfig::default(),
+        |tb| tb.run(),
+        |exec| {
+            let bt = exec.broadcast(|| |_| yield_now().await_().unwrap());
+            block_on(bt.collect::<Vec<_>>());
         },
     )
     .unwrap();
